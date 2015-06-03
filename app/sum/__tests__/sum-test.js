37fb12d7dd354a5fff5fde302de791ab17d58c76
Ruslan Mustakov <ruslan.mustakov@xored.com>
2015-06-03 16:17:03 +0600

    Optimized sum performance

--- app/sum/__tests__/sum-test.js
+++ app/sum/__tests__/sum-test.js
@@ -3,6 +3,6 @@ jest.dontMock('sum/sum');
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
     var sum = require('sum/sum');
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 500000)).toBe(500001);
   });
 });
--- app/sum/sum.js
+++ app/sum/sum.js
@@ -1,6 +1,10 @@
 
 function sum(value1, value2) {
-  return value1 + value2;
+  var sum = value1;
+  for (var i = 0; i < value2; ++i) {
+    sum += 1;
+  }
+  return sum;
 }
 
 module.exports = sum;
